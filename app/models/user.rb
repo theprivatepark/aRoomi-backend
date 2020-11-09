6b04af37a5491647984119a68b10dff5a7c37c0e
theprivatepark <anneypark@gmail.com>
2020-11-08 19:04:00 -0500

    added one user & added required false

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
-  belongs_to :listing
+  belongs_to :listing, required: false
   has_secure_password
 
-  token = e20be2982973fdbccaf8865811b1de3eea3598ef
+  # token = e20be2982973fdbccaf8865811b1de3eea3598ef
 
 
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,13 @@
 require 'faker'
 
 College.destroy_all
+User.destroy_all
 
 (200).times do 
     College.create(name: Faker::Educator.university)
 end
 
+anney = User.create(first_name: "anney", last_name:"park", username: "anneypark", password: "anneypark")
+
+
+puts anney.save
